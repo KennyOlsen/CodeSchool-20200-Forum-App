06995d81e41cd6c1ac867a9f553334936f32ee90
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-06-29 10:57:01 -0600

    Pages created

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+Vue.component("login-screen", {
+    template: `
+    <div>
+        <input v-model="emailInput" placeholder="Email">
+        <input v-model="passwordInput" placeholder="Password">
+        <button v-on:click="attemptLogin()">Login</button>
+    </div>
+    `,
+    data: function () {
+        return {
+            emailInput: '',
+            passwordInput: ''
+        }
+    },
+    methods: {
+        attemptLogin: function () {
+
+        }
+    }
+});
+
+Vue.component('register-screen', {
+    template: `
+    <div>
+        <input v-model="emailInput" placeholder="Email">
+        <input v-model="passwordInput" placeholder="Password">
+        <button v-on:click="attemptLogin()">Login</button>
+    </div>
+    `,
+    data: function () {
+        return {
+            emailInput: '',
+            passwordInput: ''
+        }
+    },
+    methods: {
+        attemptLogin: function () {
+
+        }
+    }
+});
+
 var app = new Vue({
-    el: "#app"
-})
+    el: "#app",
+    data: {
+        page: 'login'
+    }
+});
