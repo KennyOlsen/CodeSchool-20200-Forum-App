3ab8d13a2fab2b87ee46338ceb11759ff8ea2276
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-06-29 11:48:19 -0600

    User is routed to home page when logged in

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ Vue.component("login-screen", {
     template: `
     <div>
         <h2>Login</h2>
-        <input v-model="emailInput" placeholder="Email">
-        <input v-model="passwordInput" placeholder="Password">
+        <input v-model="emailInput" placeholder="Email" v-on:keydown.enter="attemptLogin()">
+        <input v-model="passwordInput" placeholder="Password" type="password" v-on:keydown.enter="attemptLogin()">
         <button v-on:click="attemptLogin()">Login</button>
     </div>
     `,
     data: function () {
         return {
             emailInput: '',
-            passwordInput: ''
+            passwordInput: '',
         }
     },
     methods: {
@@ -29,20 +29,22 @@ Vue.component("login-screen", {
                 credentials: 'include'
             });
 
+            let body = await response.json();
+            console.log(body);
 
             console.log(response);
             if (response.status == 201) {
-                console.log("Created");
-                let data = await response.json();
-                console.log("Data recieved from GET /session: " + data);
+                console.log("Login successful");
+
                 this.emailInput = '';
                 this.passwordInput = '';
+
+                app.toLoginPage();
             } else if (response.status == 401) {
-                console.log("Not logged in");
-                let data = await response.json();
-                console.log("Data recieved from GET /session: " + data);
+                console.log("Login unsuccessful");
+                this.passwordInput = '';
             } else {
-                console.log("Error: status not 200 or 401 when GETTING /session---" + response.status + response);
+                console.log("Error: status not 200 or 401 when POSTING /session---" + response.status + response);
             }
         }
     }
@@ -72,6 +74,14 @@ Vue.component('register-screen', {
     }
 });
 
+Vue.component('home-page', {
+    template: `
+    <div>
+        <h3>You are now logged in</h3>
+    </div>
+    `
+})
+
 var app = new Vue({
     el: "#app",
     data: {
@@ -81,9 +91,14 @@ var app = new Vue({
         changePage: function () {
             if (this.page == 'login') {
                 this.page = 'register';
+            } else if (this.page == 'register') {
+                this.page = 'home';
             } else {
                 this.page = 'login';
             }
+        },
+        toLoginPage: function () {
+            this.page = 'home';
         },
         getSession: async function () {
             let response = await fetch(`${URL}/session`, {
