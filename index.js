2ae4c8b42324f491aff9d384b52ae230c6d62b28
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-07-05 09:23:30 -0600

    express runs

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+app.use(express.static(`${__dirname}/public/`));
+
+app.listen(808, () => {
+    console.log("server running on port 8080");
+})
