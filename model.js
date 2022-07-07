59c316136f3c3ecaa9b156fe8b78310148c89071
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-07-07 09:51:52 -0600

    All methods work

--- model.js
+++ model.js
@@ -13,6 +13,27 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+
+const postSchema = mongoose.Schema(
+    {
+      user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      body: { type: String, required: true, default: "" },
+      thread_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thread",
+        required: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+const Post = mongoose.model("Post", postSchema);
+
+
 const threadSchema = mongoose.Schema({
         user_id: {
             type: mongoose.Schema.Types.ObjectId,
@@ -21,18 +42,19 @@ const threadSchema = mongoose.Schema({
         },
         name: {type: String, required: true, default: ""},
         description: {type: String, required: true, default: ""},
-        //posts: {type: [postSchema], required: true, default: {}},
+        posts: {type: [postSchema], required: true, default: {}},
         category: {type: String, required: true, default: ""}
-    }/*,
+    },
     {
         timestamps: true,
         //toJSON: {virtuals: true}
-    }*/
+    }
 );
 
 const Thread = mongoose.model("Thread", threadSchema);
 
 module.exports = {
     User,
-    Thread
+    Thread,
+    Post
 };
--- server/server.js
+++ server/server.js
@@ -14,44 +14,287 @@ const setupAuth = require('./auth');
 setupSessionsStore(app);
 setupAuth(app);
 
-app.post("/users", async (request, response) => {
+app.post("/users", async (req, res) => {
     try {
-        let user = await User.create ({
-            username: request.body.username,
-            fullname: request.body.fullname,
-            password: request.body.password
-        });
-        response.status(201).json(user);
+      let user = await User.create({
+        username: req.body.username,
+        fullname: req.body.fullname,
+        password: req.body.password,
+      });
+      res.status(201).json(user);
     } catch (err) {
-        response.status(400).json({
-            message: 'post request failed to create user',
+      res.status(500).json({
+        message: `post request failed to create user`,
+        error: err,
+      });
+    }
+  });
+  
+  app.get("/thread/:id", async (req, res) => {
+    // implement me :)
+    // no authentication needed a.k.a. authorization is public/open
+    let thread;
+  
+    // get the thread
+    try {
+      thread = await Thread.findById(req.params.id);
+      if (!thread) {
+        res.status(404).json({
+          message: "thread not found",
+        });
+        return;
+      }
+    } catch (err) {
+      res.status(500).json({
+        message: `get request failed to get thread`,
+        error: err,
+      });
+      return;
+    }
+  
+    // get the user
+    try {
+      thread = thread.toObject();
+      let user = await User.findById(thread.user_id, "-password"); // how would you omit the pasword?
+      thread.user = user;
+    } catch (err) {
+      console.log(
+        `unable to get user ${thread.user_id} when getting thread ${thread._id}: ${err}`
+      );
+    }
+  
+    // get the posts users
+    for (let k in thread.posts) {
+      try {
+        let user = await User.findById(thread.posts[k].user_id, "-password");
+        thread.posts[k].user = user;
+      } catch (err) {
+        console.log(
+          `unable to get user ${thread.posts[k].user_id} for post ${thread.posts[k]._id} when getting thread ${thread._id}: ${err}`
+        );
+      }
+    }
+  
+    // return the thread
+    res.status(200).json(thread);
+  });
+  
+  app.get("/threads", async (req, res) => {
+    // no authentication needed a.k.a. authorization is public/open
+    let threads;
+    // get the threads and omit the posts)
+    try {
+      threads = await Thread.find({}, "-posts");
+    } catch (err) {
+      res.status(500).json({
+        message: "list request failed to get threads",
+        error: err,
+      });
+      return;
+    }
+  
+    // get all the users for all the threads
+    for (let k in threads) {
+      try {
+        threads[k] = threads[k].toObject();
+        let user = await User.findById(threads[k].user_id, "-password"); // how would you omit the pasword?
+        threads[k].user = user;
+      } catch (err) {
+        console.log(
+          `unable to get user ${threads[k].user_id} when getting thread ${threads[k]._id}: ${err}`
+        );
+      }
+    }
+  
+    // return the threads
+    res.status(200).json(threads);
+  });
+  
+  app.post("/thread", async (req, res) => {
+    // auth
+    if (!req.user) {
+      res.status(401).json({ message: "unauthed" });
+      return;
+    }
+    // create with await + try/catch
+    try {
+      let thread = await Thread.create({
+        user_id: req.user.id,
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+      });
+      res.status(201).json(thread);
+    } catch (err) {
+      res.status(500).json({
+        message: "could not create thread",
+        error: err,
+      });
+      return;
+    }
+  });
+  
+  app.delete("/thread/:id", async (req, res) => {
+    // check if authed
+    if (!req.user) {
+      res.status(401).json({ mesage: "unauthenticated" });
+      return;
+    }
+    console.log(`request to delete a single thread with id ${req.params.id}`);
+  
+    let thread;
+  
+    // get the thread to check if the current user is allow to delete it
+    try {
+      thread = await Thread.findById(req.params.id);
+    } catch (err) {
+      res.status(500).json({
+        message: `failed to delete thread`,
+        error: err,
+      });
+      return;
+    }
+  
+    // check if we found it
+    if (thread === null) {
+      res.status(404).json({
+        message: `thread not found`,
+        thread_id: req.params.thread_id,
+      });
+      return;
+    }
+  
+    // check if the current user made the post
+    if (thread.user_id != req.user.id) {
+      res.status(403).json({ mesage: "unauthorized" });
+      return;
+    }
+  
+    // delete the post
+    try {
+      await Thread.findByIdAndDelete(req.params.id);
+    } catch (err) {
+      res.status(500).json({
+        message: `failed to delete post`,
+        error: err,
+      });
+      return;
+    }
+  
+    // return
+    res.status(200).json(thread);
+  });
+  
+  app.post("/post", async (req, res) => {
+    // check auth
+    if (!req.user) {
+      res.status(401).json({ message: "unauthed" });
+      return;
+    }
+  
+    let thread;
+  
+    // find the thread and update it with the new post
+    try {
+      thread = await Thread.findByIdAndUpdate(
+        req.body.thread_id, // what is the id
+        {
+          // what to update
+          $push: {
+            // push update operator
+            posts: {
+              // what field are we pushing to and what are we pushing?
+              user_id: req.user.id,
+              body: req.body.body,
+              thread_id: req.body.thread_id,
+            },
+          },
+        },
+        {
+          new: true, // options
+        }
+      );
+      if (!thread) {
+        res.status(404).json({
+          message: `thread not found`,
+          id: req.body.thread_id,
+        });
+        return;
+      }
+    } catch (err) {
+      res.status(500).json({
+        message: `failed to insert post`,
+        error: err,
+      });
+      return;
+    }
+  
+    // return the post
+    res.status(201).json(thread.posts[thread.posts.length - 1]);
+  });
+  
+  app.delete("/thread/:thread_id/post/:post_id", async (req, res) => {
+    // check auth
+    if (!req.user) {
+        res.status(401).json({ message: "unauthed" });
+        return;
+      }
+
+    let thread;
+    let post;
+
+    // pull thread
+    try {
+        thread = await Thread.findOne({
+        _id: req.params.thread_id,
+        "posts._id": req.params.post_id,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: `error finding thread when deleting post`,
             error: err
         });
     }
-});
 
-app.post("/thread", async (request, response) => {
-    //authenticate
-    if (!request.user) {
-        response.status(401).json({message: "unauthenticated"});
+    if (!thread) {
+        res.status(404).json({
+            message: `thread not found when deleting post`,
+            thread_id: req.params.thread_id,
+            post_id: req.params.post_id
+        });
         return
     }
-    //create with await + try/catch
+
+    // check that the post on the thread is "owned" by the requesting user (authorization)
+    // for loop over thread.posts to find the post you're looking for so you can check the user_id
+    let isSameUser = false;
+    for (let k in thread.posts) {
+        //find post
+        if (thread.posts[k]._id == req.params.post_id) {
+            post = thread.posts[k];
+            if (thread.posts[k].user_id == req.user.id) {
+                isSameUser = true;
+            }
+        }
+        //check user id
+    }
+    // delete the post
     try {
-        let thread = await Thread.create({
-            user_id: request.user.id,
-            name: request.body.name,
-            description: request.body.description,
-            category: request.body.category
+        await Thread.findByIdAndUpdate(req.params.thread_id, {
+        $pull: {
+            posts: {
+            _id: req.params.post_id,
+            },
+        },
         });
-        response.status(201).json(thread);
+        // return the deleted post
     } catch (err) {
-        response.status(500).json({
-            message: "could not create thread",
+        res.status(501).json({
+            message: `error deleting post`,
             error: err
         });
+        return
     }
-
 });
 
 module.exports = app;
